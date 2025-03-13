8b5a63d83cc1c0d9dbadfbcf4936d0d3343b3eeb
raghavsharma30 <138058029+raghavsharma30@users.noreply.github.com>
2025-03-13 22:42:19 +0530

    Update game.h

--- Server/game.h
+++ Server/game.h
@@ -1,1 +1,35 @@
+#ifndef GAME_H
+#define GAME_H
+#include <string>
+#include <set>
+using namespace std;
+
+const int BOARD_SIZE = 7;
+enum class Player{ NONE, BLACK, WHITE };
+class Game{
+private : 
+	Player** board;
+	Player currentTurn;
+	int blackCaptures;
+	int whiteCaptures;
+	bool gameover;
+	Player winner;
+	const int captureGoal;
+	int countLiberties(int x, int y, set<pair<int, int>>& visited, Player player);
+	bool isValidMove(int x, int y, Player player);
+	void captureStones(int x, int y, Player opponent);
+	void switchTurn();
+
+public: 
+	Game(int goal = 1);
+	~Game();
+	bool makeMove(int x. int y);
+	void displayBoard() const;
+	bool isGameOver() const{ return gameOver;}
+	Player getWinner() const{ return winner;}
+	Player getCurrentTurn(){ return currentTurun; }
+	string serialize(const string& state);
+	pair<int, int>* getEmptyPositions(int& count) const;
+};
+#endif
 
