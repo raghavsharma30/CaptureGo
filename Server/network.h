f619b02257116d497bc5d8056f31adf5d847a85c
raghavsharma30 <138058029+raghavsharma30@users.noreply.github.com>
2025-03-17 16:02:35 +0530

    fixed errors

--- Server/network.h
+++ Server/network.h
@@ -1,19 +1,15 @@
+
 #ifndef NETWORK_H
 #define NETWORK_H
 #include <string>
 using namespace std;
 class Network {
 public:
-    Network();
-    ~Network();
     int createServerSocket(int port);
+    int createClientSocket(const string& serverIp, int port);
     int acceptClient(int serverSocket);
-    int createClientSocket(const std::string& serverIp, int port);
-    bool sendData(int socket, const std::string& data);
-    std::string receiveData(int socket);
+    void sendData(int socket, const string& data);
+    string receiveData(int socket);
     void closeSocket(int socket);
-private:
-    void initialize();
-    void cleanup();
 };
 #endif
