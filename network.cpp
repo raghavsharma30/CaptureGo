c52d41be6e8680191eabc6fb3f634e5d4cec4b95
raghavsharma30 <138058029+raghavsharma30@users.noreply.github.com>
2025-03-21 17:49:31 +0530

    final fixed code

--- network.cpp
+++ network.cpp
@@ -1,50 +1,50 @@
 #include "network.h"
-#include <iostream>
-#include <cstring>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <iostream>
+#include <string.h>
 using namespace std;
-int Network::createServerSocket(int port) {
+int Network::createClientSocket(const string& hostname, int port) {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) return -1;
-    sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(port);
-    if (bind(sock, (sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) return -1;
-    if (listen(sock, 5) < 0) return -1;
+    if (sock < 0) {
+        cout << "Failed to create client socket: " << strerror(errno) << "\n";
+        return -1;
+    }
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(hostname.c_str());
+    if (connect(sock, (sockaddr*)&addr, sizeof(addr)) < 0) {
+        cout << "Connect failed: " << strerror(errno) << "\n";
+        close(sock);
+        return -1;
+    }
     return sock;
 }
-
-int Network::createClientSocket(const string& serverIp, int port) {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) return -1;
-    sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port);
-    inet_pton(AF_INET, serverIp.c_str(), &serverAddr.sin_addr);
-    if (connect(sock, (sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) return -1;
-    return sock;
-}
-
-int Network::acceptClient(int serverSocket) {
-    sockaddr_in clientAddr;
-    socklen_t addrLen = sizeof(clientAddr);
-    return accept(serverSocket, (sockaddr*)&clientAddr, &addrLen);
-}
-
-void Network::sendData(int socket, const string& data) {
-    send(socket, data.c_str(), data.length(), 0);
-}
-
 string Network::receiveData(int socket) {
     char buffer[1024] = {0};
-    int bytesRead = recv(socket, buffer, sizeof(buffer) - 1, 0);
-    if (bytesRead <= 0) return "";
-    return string(buffer, bytesRead);
+    int bytes = recv(socket, buffer, sizeof(buffer) - 1, 0);
+    return bytes > 0 ? string(buffer, bytes) : "";
 }
-
+void Network::sendData(int socket, const string& data) {
+    send(socket, data.c_str(), data.size(), 0);
+}
 void Network::closeSocket(int socket) {
     close(socket);
+}
+vector<string> split(const string& s, char delimiter) {
+    vector<string> tokens;
+    string token;
+    for (char c : s) {
+        if (c == delimiter && !token.empty()) {
+            tokens.push_back(token);
+            token.clear();
+        } else if (c != delimiter) {
+            token += c;
+        }
+    }
+    if (!token.empty()) tokens.push_back(token);
+    return tokens;
 }
