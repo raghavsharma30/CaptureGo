de05911d0c4606fb4d25ff430baca4f6f35a2ad0
raghavsharma30 <138058029+raghavsharma30@users.noreply.github.com>
2025-03-21 11:56:57 +0000

    final fixed code

--- network.h
+++ network.h
@@ -1,15 +1,14 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 #include <string>
+#include <vector>
 using namespace std;
 class Network {
 public:
-    int createServerSocket(int port);
-    int createClientSocket(const string& serverIp, int port);
-    int acceptClient(int serverSocket);
-    void sendData(int socket, const string& data);
-    string receiveData(int socket);
-    void closeSocket(int socket);
+    static int createClientSocket(const string& hostname, int port);
+    static string receiveData(int socket);
+    static void sendData(int socket, const string& data);
+    static void closeSocket(int socket);
 };
-
+vector<string> split(const string& s, char delimiter);
 #endif
